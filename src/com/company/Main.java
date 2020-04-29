4cc2c58d5f32ac067e4f9b049ef96467f7233735
Anton <sogonyaevjk@mail.ru>
2020-04-29 14:22:24 +0300

    с выводом

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,39 +8,65 @@ public class Main {
        String j= "";
 
 
+
     public String func(String[] value, int[]chance){
             int sum = 0;
-            for(int i = 0; i< chance.length; i++){
-                sum+= chance[i];
+            for (int i = 0; i < chance.length; i++) {
+                sum += chance[i];
 
             }
 
             StringBuilder sb = new StringBuilder();
-            for(int i = 0;i < value.length; i++){
+            for (int i = 0; i < value.length; i++) {
 
-                int iNumber = (int)(Math.random() * ++sum) + 1;
+                int iNumber = (int) (Math.random() * ++sum) + 1;
 
 
-                if(iNumber>=chance[i] && chance[i]<iNumber){
-                    sb.append(value[i]+ " ,");
+                if (iNumber >= chance[i] && chance[i] < iNumber) {
+                    sb.append(value[i] + " ,");
                     j = j + sb;
+
                 }
 
             }
             n++;
             return j;
 
-
-
-
-
         }
+
+
 
     public static void main(String[] args) {
             Main mn = new Main();
-        String func = mn.func(new String[]{"а","b","c"}, new int[]{10,61,30});
-        System.out.println(func);
+            int n= 0;
+            int a = 0;
+            int b=0;
+            int c=0;
+            while(n<1000) {
+                String func = mn.func(new String[]{"а", "b", "c"}, new int[]{2, 5, 10});
+                int[] arr = new int[func.length()];
 
+                for (int i = 0; i < func.length(); i++) {
+                    arr[i]= func.charAt(i);
+                }
+
+                for (int i = 0; i < arr.length; i++) {
+                    if(func.charAt(i)=='a'){
+                        a++;
+                    }
+                    if(func.charAt(i)=='b'){
+                        b++;
+                    }
+                    if(func.charAt(i)=='c'){
+                        c++;
+                    }
+                }
+
+
+                n++;
+            }
+
+        System.out.println(a+" "+b+" "+c);
     }
 
 
