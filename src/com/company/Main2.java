ec6aa8aab8ff09ccad383f2a2a52ae21b451650b
Anton <sogonyaevjk@mail.ru>
2020-04-30 15:50:08 +0300

    aa

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -6,54 +6,32 @@ import java.util.Random;
 
 public class Main2 {
 
-
-
-
     public static String func(String[] value, int[]chance){
         String j= "";
         int sum = 0;
         int rand = value.length;
         for (int i = 0; i < chance.length; i++) {
             sum += chance[i];
-
         }
-
         StringBuilder sb = new StringBuilder();
 
            int iNumber = 1 + (int) (Math.random() * sum);
            // int iNumber = (int) (Math.random() * ++sum) + 1;
             int Inter = (int) (Math.random() * ++rand) + 1;
 
-
-
-
-
              for(int i = 1; i< value.length; i++) {
                  if(chance[0] > iNumber ){
-
                      sb.append(value[0]);
                      j = j + sb;
-
                      return j;
                  } else if((chance[i-1] <= iNumber &&  iNumber < chance[i])){
-
                      sb.append(value[i]);
                      j = j + sb;
-
                      return j;
                  }
              }
         return j;
-
             }
-
-
-
-
-
-
-
-
 
     public static void main(String[] args) {
         int n = 0;
@@ -62,7 +40,6 @@ public class Main2 {
         int c = 0;
         while (n < 1000) {
             String func = func(new String[]{"a", "b", "c"}, new int[]{10, 0, 0});
-
             for (int i = 0; i < func.length(); i++) {
                 switch (func) {
                     case ("a"):
@@ -74,16 +51,14 @@ public class Main2 {
                     case ("c"):
                         c++;
                         break;
-
                 }
             }
             n++;
         }
-
         System.out.println("a = " + a + " b = " + b + " c = " + c);
-
     }
-
-
+    public static String funcA(){
+        return "a";
+    }
 }
 
