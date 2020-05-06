116347094acc54e3883c151e651d76e6f2a52ccf
Anton <sogonyaevjk@mail.ru>
2020-05-06 12:40:30 +0300

    next6

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -6,8 +6,8 @@ import java.util.Random;
 
 public class Main2 {
 
-    public static String func(String[] value, int[]chance){
-        String j= "";
+    public static String func(String[] value, int[] chance) {
+        String j = "";
         int sum = 0;
         int rand = value.length;
         for (int i = 0; i < chance.length; i++) {
@@ -15,23 +15,23 @@ public class Main2 {
         }
         StringBuilder sb = new StringBuilder();
 
-           int iNumber = 1 + (int) (Math.random() * sum);
-           // int iNumber = (int) (Math.random() * ++sum) + 1;
-            int Inter = (int) (Math.random() * ++rand) + 1;
+        int iNumber = 1 + (int) (Math.random() * sum);
+        // int iNumber = (int) (Math.random() * ++sum) + 1;
+        int Inter = (int) (Math.random() * ++rand) + 1;
 
-             for(int i = 1; i< value.length; i++) {
-                 if(chance[0] > iNumber ){
-                     sb.append(value[0]);
-                     j = j + sb;
-                     return j;
-                 } else if((chance[i-1] <= iNumber &&  iNumber < chance[i])){
-                     sb.append(value[i]);
-                     j = j + sb;
-                     return j;
-                 }
-             }
-        return j;
+        for (int i = 1; i < value.length; i++) {
+            if (chance[0] > iNumber) {
+                sb.append(value[0]);
+                j = j + sb;
+                return j;
+            } else if ((chance[i - 1] <= iNumber && iNumber < chance[i])) {
+                sb.append(value[i]);
+                j = j + sb;
+                return j;
             }
+        }
+        return j;
+    }
 
     public static void main(String[] args) {
         int n = 0;
@@ -40,7 +40,7 @@ public class Main2 {
         int c = 0;
         while (n < 1000) {
             String func = func(new String[]{"a", "b", "c"}, new int[]{10, 0, 0});
-            for (int i = 0; i < func.length(); i++) {
+          /*  for (int i = 0; i < func.length(); i++) {
                 switch (func) {
                     case ("a"):
                         a++;
@@ -56,7 +56,7 @@ public class Main2 {
             n++;
         }
         System.out.println("a = " + a + " b = " + b + " c = " + c);
-
+*/
 
       /*  int n = 0;
         int a = 0;
@@ -81,11 +81,12 @@ public class Main2 {
         }
         System.out.println("a = " + a + " b = " + b + " c = " + c);
 */
-    }
-    public static String funcA(String [] enter){
+        }
+   /* public static String funcA(String [] enter){
         int lenenter = enter.length;
         int iNumber2 = 0 + (int) (Math.random() * lenenter); ;
         return enter[iNumber2];
+    }*/
     }
 }
 
